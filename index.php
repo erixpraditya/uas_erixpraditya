aa7f1c08532c1368301968735792bb5afcbe3c63
erixpraditya <erixpradityaa@gmail.com>
2025-01-22 14:34:36 +0700

    membuat koneksi, index, navbar

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Uas Erix</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <?php
+         include_once('navbar.php');
+    ?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+        <div class="card-header text-center">
+            <h3>Sistem Informasi Mahasiswa</h3>
+        </div>
+        <div class="card-body text-center">
+                <h4>Selamat Datang</h4>
+                <p>Selamat Datang di Halaman Utama Sistem Informasi Mahasiswa LP3I Banda Aceh!
+                 Di sini, Anda dapat mengakses informasi terkait perkuliahan, absensi, nilai, dan berbagai pengumuman penting lainnya.
+                 Pastikan Anda selalu memanfaatkan sistem ini untuk mendukung kelancaran studi Anda.</p>
+            </div>
+         </div>
+
+        </div>
+    </div>
+</div>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
